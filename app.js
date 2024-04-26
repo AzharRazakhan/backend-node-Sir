95243be9a235c990f5312e1e624bd3134e4dbad9
Thoughtwin <azharrazakhan78611>
2024-04-26 15:57:23 +0530

    add cluster

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const cors = require('cors');
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
-.then(() => console.log('Connected!'));
+// mongoose.connect('mongodb://127.0.0.1:27017/test')
+// .then(() => console.log('Connected!'));
+
+mongoose.connect('mongodb+srv://azharkhan:MU0Lg49qMsSS4dpD@cluster0.5t3dzm6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => console.log('Cluster connection'))
 app.use(cors());
 
 
